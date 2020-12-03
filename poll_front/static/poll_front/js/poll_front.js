3e2b55430f9272ebc1ef81e0a1d5ab3b871bcf45
youngcoconut <wilfunkyarceno@gmail.com>
2020-12-04 02:17:07 +0800

    added static to shit

--- poll_front/static/poll_front/js/poll_front.js
+++ poll_front/static/poll_front/js/poll_front.js
@@ -9,13 +9,10 @@ const load_polls = async () => {
 $(document).ready(() => {
   setTimeout(() => {
     const promise = new Promise((resolve, reject) => {
-      $.getScript(
-        `${urls.static}/poll_front/js/polls-feed.js`,
-        function (script) {
-          initiate_poll_feed();
-          resolve(200);
-        }
-      );
+      $.getScript(`static/poll_front/js/polls-feed.js`, function (script) {
+        initiate_poll_feed();
+        resolve(200);
+      });
     });
     promise.then((response) => {
       load_polls();
