7131ea0a1979cc3f0824dc3d8ddad05ea2fca801
youngcoconut <wilfunkyarceno@gmail.com>
2020-12-04 17:42:48 +0800

    modified the poll_front.js to be the same as the production build one

--- poll_front/static/poll_front/js/poll_front.js
+++ poll_front/static/poll_front/js/poll_front.js
@@ -1,3 +1,5 @@
+console.log(urls.static);
+
 const load_polls = async () => {
   response = await api_get_req({
     api_link: api.type.question,
@@ -7,20 +9,5 @@ const load_polls = async () => {
   initiate_survey(data.questions);
 };
 $(document).ready(() => {
-<<<<<<< HEAD
-  const promise = new Promise((resolve, reject) => {
-    $.getScript(
-      `${urls.static}/poll_front/js/polls-feed.js`,
-      function (script) {
-        initiate_poll_feed();
-        resolve(200);
-      }
-    );
-  });
-  promise.then((response) => {
-    load_polls();
-  });
-=======
   load_polls();
->>>>>>> 1fe7c1ce85ab8b4aad5a2cf010ea3d0b4b6f81d2
 });
