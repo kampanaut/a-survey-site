c7eaecc6153c499733a678abf632200ab09ab160
youngcoconut <wilfunkyarceno@gmail.com>
2020-12-04 19:19:43 +0800

    js files linking hotfix

--- poll_front/static/poll_front/js/polls-feed.js
+++ poll_front/static/poll_front/js/polls-feed.js
@@ -1,17 +1,17 @@
 const initiate_poll_feed = () => {
-    $.getScript(`${urls.static}/poll_front/js/poll-component.js`, function (script) {
-        poll_component();
-    });
+  $.getScript(
+    `${urls.static}poll_front/js/poll-component.js`,
+    function (script) {
+      poll_component();
+    }
+  );
 };
 const survey_navi_btn_check = (questions) => {
-    const left_btn_container = $('div.poll-navi-btns_container.left');
-    // const right_btn_container = $('div.poll-navi-btns_container.right');
-    if (questions.sort == 1)
-    {
-        $(left_btn_container).attr('show', 'false');
-    }
-    else
-    {
-        $(left_btn_container).attr('show', 'true');
-    }
-}
+  const left_btn_container = $("div.poll-navi-btns_container.left");
+  // const right_btn_container = $('div.poll-navi-btns_container.right');
+  if (questions.sort == 1) {
+    $(left_btn_container).attr("show", "false");
+  } else {
+    $(left_btn_container).attr("show", "true");
+  }
+};
