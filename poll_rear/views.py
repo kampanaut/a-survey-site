33a5201eea257a2ab2daef56e83cf62c51787249
youngcoconut <wilfunkyarceno@gmail.com>
2020-12-06 23:45:15 +0800

    fixed the soting issue I just found in the ui
    I thought I fixed it, so yeah

--- poll_rear/views.py
+++ poll_rear/views.py
@@ -12,7 +12,8 @@ class QuestionsFetch(View):
     template_name = 'poll_front/poll-module.html'
 
     def get(self, request, *args, **kwargs):
-        questions = Question.objects.order_by('sort')
+        questions = Question.objects.order_by('-sort')
+        questions_dict = dict()
         questions_arr = list()
         question_tup = tuple()
         context = {
@@ -20,39 +21,16 @@ class QuestionsFetch(View):
         }
 
         for a_question in questions:
-            question_tup = tuple(
-                [
-                    a_question.pk, tuple(
-                        [
-                            tuple(
-                                ["text", a_question.question_text]
-                            ),
-                            tuple(
-                                ["id", a_question.pk]
-                            ),
-                            tuple(
-                                ["sort", a_question.sort]
-                            )
-                        ]
-                    )
-                ]
-            )
-            questions_arr.append(question_tup)
-
-        question_keys = dict()
-        i = 0
-        for keypk, data in questions_arr:
-            question_keys[keypk] = list()
-            for subkey in data:
-                question_keys[keypk].append({
-                    subkey[0]: subkey[1]
-                })
-
-            question_keys[keypk] = {**question_keys[keypk][0], **
-                                    question_keys[keypk][1], **question_keys[keypk][2]}
+            questions_dict.update({
+                a_question.sort: {
+                    "text": a_question.question_text,
+                    "sort": a_question.sort,
+                    "id": a_question.id
+                }
+            })
 
         return JsonResponse({
-            'questions': question_keys
+            'questions': questions_dict
         })
 
 
