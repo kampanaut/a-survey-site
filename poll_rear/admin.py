7c0de25e3610f3723d23310079c3dff7e97a2de8
youngcoconut <wilfunkyarceno@gmail.com>
2021-01-04 00:01:27 +0800

    First changes and improvement on the Admin panel
    I'm saying that this is just the first change. I'm not sure if my changes would come up in such high intuitiveness for Lois. I just don't want to make her suffer so that's why care about this kinda useless portion of development. But still it's necessary for this is the fucking site where we would just bang our heads into.

--- poll_rear/admin.py
+++ poll_rear/admin.py
@@ -4,10 +4,49 @@ from .models import *
 # Register your models here.
 
 
-class QuestionAdminForm(admin.ModelAdmin):
+class AdminStyleUtils(object):
+    class ParticipantStyle():
+        def combine_name(instance):
+            return f"""{instance.first_name} {instance.last_name}"""
+
+        class AnswerInline(admin.StackedInline):
+            model = Answer
+            extra = 0
+
+    class AnswerStyle():
+        def return_obj_identifier(instance):
+            return instance.__str__()
+
+
+class QuestionAdminStyle(admin.ModelAdmin):
     fields = ['sort', 'question_text']
+    list_display = ('question_text', 'sort')
 
 
-admin.site.register(Participant)
-admin.site.register(Question, QuestionAdminForm)
-admin.site.register(Answer)
+class ParticipantAdminStyle(admin.ModelAdmin):
+    AdminStyleUtils.ParticipantStyle.combine_name.short_description = 'Name'
+    list_display = (AdminStyleUtils.ParticipantStyle.combine_name,
+                    'birthday', 'sex', 'id')
+    fieldsets = ([
+        ('Main Details', {
+            'fields': [
+                'first_name',
+                'last_name',
+                'birthday',
+                'sex'
+            ]
+        }
+        ),
+    ])
+    inlines = [AdminStyleUtils.ParticipantStyle.AnswerInline]
+
+
+class AnswerAdminStyle(admin.ModelAdmin):
+    AdminStyleUtils.AnswerStyle.return_obj_identifier.short_description = 'Details'
+    list_display = [AdminStyleUtils.AnswerStyle.return_obj_identifier,
+                    'id', 'participant', 'question', 'answer']
+
+
+admin.site.register(Participant, ParticipantAdminStyle)
+admin.site.register(Question, QuestionAdminStyle)
+admin.site.register(Answer, AnswerAdminStyle)
